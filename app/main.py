66b11a3a2f3e98fefb632925bb87846139a8f3dd
neeeksa <avgust.k31@gmail.com>
2024-12-06 19:52:09 +0000

    First_Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,58 @@
-# write your code here
+from typing import List
+
+
+class SoftwareEngineer:
+    def __init__(self, name: str, skills = None) -> None:
+        if skills is None:
+            skills = []
+        self.name = name
+        self.skills = skills
+
+    def learn_skill(self, skill: str) -> None:
+        self.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str, skills = None) -> None:
+        if skills is None:
+            skills = ["JavaScript", "HTML", "CSS"]
+        super().__init__(name, skills)
+
+    def create_awesome_web_page(self) -> str:
+        print(f"{self.name} is creating a webpage...")
+        return "<h1>Hello world</h1>"
+
+
+class BackendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str, skills = None) -> None:
+        if skills is None:
+            skills = ["Python", "SQL", "Django"]
+        super().__init__(name, skills)
+
+    def create_powerful_api(self) -> str:
+        print(f"{self.name} is creating an API...")
+        return "http://127.0.0.1:8000"
+
+
+class AndroidDeveloper(SoftwareEngineer):
+    def __init__(self, name: str, skills = None) -> None:
+        if skills is None:
+            skills = ["Java", "Android studio"]
+        super().__init__(name, skills)
+
+    def create_smooth_mobile_app(self) -> str:
+        print(f"{self.name} is creating a mobile app...")
+        return "Ads every three swipes"
+
+
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
+    def __init__(self, name: str, skills = None) -> None:
+        if skills is None:
+            skills = ["Python", "SQL", "Django", "JavaScript", "HTML", "CSS", ]
+        FrontendDeveloper.__init__(self, name, skills)
+        BackendDeveloper.__init__(self, name, skills)
+
+    def create_web_application(self) -> None:
+        print(f"{self.name} started creating a web application...")
+        super().create_powerful_api()
+        super().create_awesome_web_page()
